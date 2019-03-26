b6acc9bd35e38086aba6eec8d24effd396968ef6
AditMeh <40676295+AditMeh@users.noreply.github.com>
2019-03-26 17:05:43 -0400

    Update Video_Detection.py

--- Video_Detection.py
+++ Video_Detection.py
@@ -1,4 +1,4 @@
-#this is the library that we're using for the object detection
+#this is the library that we're using for the object detection. It was created by deepquest AI
 from imageai.Detection import VideoObjectDetection
 import os
 
@@ -14,15 +14,15 @@ detector.setModelTypeAsRetinaNet()
 #Set the model path of the model file we downloaded (the resnet model that uses the COCO database)
 detector.setModelPath(os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
 
-#Load the model
+#Load the model to begin processing
 detector.loadModel()
 
 # This takes each frame from the video and detects each object inside of the frame
-#After doing so, it pareses the images together in to an output video at 20 frames per second. The output file is in AVI
+#After doing so, it parses the images together in to an output video at 20 frames per second. The output file is in AVI
 # it takes the input file and stores the output file in the same folder.
 path = detector.detectObjectsFromVideo(input_file_path=os.path.join(execution_path, "waste.mp4"),
                                 output_file_path=os.path.join(execution_path, "Detected_Output")
                                 , frames_per_second=20, log_progress=False)
 
-#For me to make sure the model
+#For me to make sure the model goes into the right folder
 print(path)
