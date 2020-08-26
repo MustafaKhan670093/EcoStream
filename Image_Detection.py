04203a7db85f9a0a1f1ed90f50c79ba41df1bd73
Mustafa Khan <48969731+MustafaKhan670093@users.noreply.github.com>
2020-08-26 16:45:09 -0400

    Update Image_Detection.py

--- Image_Detection.py
+++ Image_Detection.py
@@ -1,30 +1,30 @@
-#Import the object detection class. Image AI is an object detection library developed by DeepQuest AI
+#Import the object detection class. Image AI is an object detection library developed by DeepQuest AI.
 from imageai.Detection import ObjectDetection
 import os
 
-#returns a working directory for the actual folder of the file
+#The following line of code returns a working directory for the actual folder of the file.
 execution_path = os.getcwd()
 
-#Initialize the detector
+#Initialize the detector.
 detector = ObjectDetection()
 
-#this sets the initial object detection model instance to the pre trained "RetinaNet" model. This was downloaded from a whl
+#This sets the initial object detection model instance to the pre trained "RetinaNet" model. 
 detector.setModelTypeAsRetinaNet()
 
-#Set the model path of the model file we downloaded (the resnet model that uses the COCO database)
+#Set the model path of the model file we downloaded (the resnet model that uses the COCO database).
 detector.setModelPath(os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
 
-#Load the model
+#Load the model.
 detector.loadModel()
 
-#analyzing the image and then creating a file that highlights the isolated objects in the initial folder
+#Analyzing the image and then creating a file that highlights the isolated objects in the initial folder.
 detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "input.jpg"),
                                              output_image_path=os.path.join(execution_path , "output.jpg"))
 
-#compare the image to the images stored in the COCO database and find the percentage of similarity
+#Compare the image to the images stored in the COCO database and find the percentage of similarity.
 for x in detections:
     print(x["name"] , " : " , x["percentage_probability"] )
 
-#create the new image and store it in the filepath
+#Create the new image and store it in the filepath.
 from IPython.display import Image
 Image("output.png")
